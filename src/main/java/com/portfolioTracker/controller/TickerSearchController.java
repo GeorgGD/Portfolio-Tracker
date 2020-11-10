c0d6adbc0e3dbf3ad38bbbe6683becabd325485c
GeorgGD <giorgos_davak@hotmail.com>
2020-11-10 19:55:13 +0100

    Implement tickerSearch for the case of empty string

--- src/main/java/com/portfolioTracker/controller/TickerSearchController.java
+++ src/main/java/com/portfolioTracker/controller/TickerSearchController.java
@@ -1,13 +1,25 @@
 package com.portfolioTracker.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TickerSearchController {
 
-	public String tickerSearch(@RequestParam("ticker") String ticker) {
+	@RequestMapping("tickerSearch")
+	public ModelAndView tickerSearch(@RequestParam("ticker") String ticker) {
+		// TODO: pass ticker to an api class that makes the api call
 
-		return null;
+		// TODO: return the name of the company and current price
+		String res;
+		ModelAndView mav = new ModelAndView();
+		
+		if(ticker == null || ticker.equals("")) {
+			return null;
+		}
+		
+		return mav;
 	}
 }
