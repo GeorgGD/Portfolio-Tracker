715409dc426bc81b9c39eb3147d08edc1d112869
GeorgGD <giorgos_davak@hotmail.com>
2020-11-11 10:27:27 +0100

    Introduce currentPrice

--- src/main/java/com/portfolioTracker/api/YahooAPIRequester.java
+++ src/main/java/com/portfolioTracker/api/YahooAPIRequester.java
@@ -1,5 +1,12 @@
 package com.portfolioTracker.api;
 
 public class YahooAPIRequester implements APIRequester{
-	
+
+	public String currentPrice(String ticker) {
+		// send get request
+
+		// select the desired data
+		
+		return null;
+	}
 }
