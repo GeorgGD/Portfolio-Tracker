1058e7fb673b3a9d3fed8835f253c29b43bd5199
GeorgGD <giorgos_davak@hotmail.com>
2020-11-11 11:30:10 +0100

    Add documentation

--- src/main/java/com/portfolioTracker/api/APIRequester.java
+++ src/main/java/com/portfolioTracker/api/APIRequester.java
@@ -1,8 +1,25 @@
 package com.portfolioTracker.api;
 
+/**
+ * The following interface is meant to allow developers to switch the API
+ * they are using in the background without affecting classes using  
+ * APIRequester. 
+ * @author Georgios Davakos
+ * @since 2020-11-11
+ */
 public interface APIRequester {
 
+	/**
+	 * Looks up the current price of a stock for a given ticker symbol
+	 * @param ticker The ticker of the stock
+	 * @return The currentPrice of a stock	
+	 */
 	public String currentPrice(String ticker);
 
+	/**
+	 * The name of the stock with the given tocker symbol
+	 * @param ticker The ticker of the stock
+	 * @return The name of the stock	
+	 */
 	public String stocksName(String ticker);
 }
