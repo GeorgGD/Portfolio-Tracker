7c560cf95d24b5a6ecd689a0818caa3cc4608882
GeorgGD <giorgos_davak@hotmail.com>
2020-11-14 17:41:49 +0100

    Introduce viewResolver

--- src/main/java/config/SpringConfig.java
+++ src/main/java/config/SpringConfig.java
@@ -1,10 +1,24 @@
 package config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan({"com.portfolioTracker.api", "com.portfolioTracker.controller"})
 public class SpringConfig {
-	
+
+	/**
+	 * The following method figures out which view the dispatcher should pick
+	 * @return The view resolver	
+	 */
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver view = new InternalResourceViewResolver();
+		view.setPrefix("/WEB-INF/view/");
+		view.setSuffix(".jsp");
+		return view;
+	}
 }
