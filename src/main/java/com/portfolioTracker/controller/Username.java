b028ecac833d3f1608466b5f459271b3ce67f2ec
GeorgGD <giorgos_davak@hotmail.com>
2020-11-15 19:02:11 +0100

    Add viewHandler

--- src/main/java/com/portfolioTracker/controller/Username.java
+++ src/main/java/com/portfolioTracker/controller/Username.java
@@ -2,6 +2,9 @@ package com.portfolioTracker.controller;
 
 import javax.servlet.http.HttpServletResponse;
 
+import com.portfolioTracker.view.ViewHandler;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,10 +14,15 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class Username {
 
+	@Autowired
+	private ViewHandler viewHandler;
+	
 	@RequestMapping(value = "/username", method = RequestMethod.POST)
 	public ModelAndView setUsernameCookie(@RequestParam("userName") String username, HttpServletResponse response) {
 		//If username is null return to index
-
+		if(username == null || username.equals("")) {
+			
+		}
 		//Else create a cookie with username and send them to portfolio page
 		return null;
 	}
