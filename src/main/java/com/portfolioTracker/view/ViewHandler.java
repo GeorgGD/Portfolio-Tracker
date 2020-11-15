7d6f89f0286a4caeff3e6bf833c10e7eb79d3edc
GeorgGD <giorgos_davak@hotmail.com>
2020-11-15 16:44:30 +0100

    Introduce ViewHandler

---
+++ src/main/java/com/portfolioTracker/view/ViewHandler.java
@@ -0,0 +1,11 @@
+package com.portfolioTracker.view;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class ViewHandler {
+
+	private ModelAndView mav;
+
+}
