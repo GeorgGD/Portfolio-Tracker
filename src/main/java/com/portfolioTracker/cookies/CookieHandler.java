e0a4649b0f4cb38ef56a02cf37986cf237842e7e
GeorgGD <giorgos_davak@hotmail.com>
2020-11-16 12:23:16 +0100

    Add documentation

--- src/main/java/com/portfolioTracker/cookies/CookieHandler.java
+++ src/main/java/com/portfolioTracker/cookies/CookieHandler.java
@@ -4,6 +4,11 @@ import javax.servlet.http.Cookie;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * The following class is meant to manage cookies
+ * @author Georgios Davakos
+ * @since 2020-11-16
+ */
 @Component
 public class CookieHandler {
 
@@ -17,12 +22,19 @@ public class CookieHandler {
 		return cookie;
 	}
 
+	/**
+	 * Sets the expire time of the cookie
+	 * @param age The max age	
+	 */
 	public void addAge(int age) {
 		if(cookie != null) {
 			cookie.setMaxAge(age);
 		}
 	}
 
+	/**
+	 * Sets the expire time to one week	
+	 */
 	public void oneWeekCookie() {
 		if(cookie != null) {
 			cookie.setMaxAge(60*60*24*7);
