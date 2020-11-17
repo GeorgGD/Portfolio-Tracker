06b0c6ac7c120758a46fb3a98b6d24a5f812244f
GeorgGD <giorgos_davak@hotmail.com>
2020-11-17 16:55:45 +0100

    Introduce TickerNotFoundException

---
+++ src/main/java/com/portfolioTracker/controller/TickerNotFoundException.java
@@ -0,0 +1,13 @@
+package com.portfolioTracker.controller;
+
+public class TickerNotFoundException extends Exception {
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public TickerNotFoundException(String s) {
+		super(s);
+	}
+}
