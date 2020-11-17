db1737b1ddf09159d74bbb4e5ca86b6a4538797b
GeorgGD <giorgos_davak@hotmail.com>
2020-11-17 13:11:45 +0100

    Introduce portfolioSearch

--- src/main/java/com/portfolioTracker/controller/PortfolioSearch.java
+++ src/main/java/com/portfolioTracker/controller/PortfolioSearch.java
@@ -1,19 +1,32 @@
 package com.portfolioTracker.controller;
 
+import com.portfolioTracker.api.APIRequester;
 import com.portfolioTracker.cookies.CookieHandler;
 import com.portfolioTracker.view.ViewHandler;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PortfolioSearch {
 
 	@Autowired
+	private APIRequester api;
+	
+	@Autowired
 	private ViewHandler viewHandler;
 
 	@Autowired
 	private CookieHandler cookieHandler;
 
+	@RequestMapping(value = "portfolioSearch", method = RequestMethod.GET)
+	public ModelAndView portfolioSearch(@RequestParam("ticker") String ticker, @RequestParam("sharesNum") String numShares, @RequestParam("buyInPrice") String buyInPrice) {
+		
+		
+		return null;
+	}
 }
