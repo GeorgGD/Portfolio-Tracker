19a3a8b4250030a98c5db47bb25427e33962a2dc
GeorgGD <giorgos_davak@hotmail.com>
2020-11-17 14:31:20 +0100

    Add soft implementation of portfolioSearch

--- src/main/java/com/portfolioTracker/controller/PortfolioSearch.java
+++ src/main/java/com/portfolioTracker/controller/PortfolioSearch.java
@@ -1,5 +1,7 @@
 package com.portfolioTracker.controller;
 
+import java.util.HashMap;
+
 import com.portfolioTracker.api.APIRequester;
 import com.portfolioTracker.cookies.CookieHandler;
 import com.portfolioTracker.view.ViewHandler;
@@ -23,10 +25,33 @@ public class PortfolioSearch {
 	@Autowired
 	private CookieHandler cookieHandler;
 
-	@RequestMapping(value = "portfolioSearch", method = RequestMethod.GET)
+	@RequestMapping(value = "/portfolioSearch", method = RequestMethod.GET)
 	public ModelAndView portfolioSearch(@RequestParam("ticker") String ticker, @RequestParam("sharesNum") String numShares, @RequestParam("buyInPrice") String buyInPrice) {
+		HashMap<String, String> expValuePair = new HashMap<String, String>();
+	    String view = "portfolio";
+		ModelAndView mav;
+		viewHandler.setModelView(new ModelAndView());
 		
+		int shares;
+		double price;
+
+		try {
+			shares = Integer.parseInt(numShares);
+			price = Double.parseDouble(buyInPrice);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+			return null;
+		}
+		// Prep current investment and total investment
+
+		// Make api request to get name and currentPrice
+	    expValuePair.put("name", api.nameOfCompany(ticker));
+		expValuePair.put("shares", numShares);
+		expValuePair.put("buyInPrice", buyInPrice);
 		
-		return null;
+		expValuePair.put("currentInvestment", "0 USD");
+		expValuePair.put("currentEvaluation", "0 USD");
+		mav = viewHandler.setupModelAndView(expValuePair, view);
+		return mav;
 	}
 }
