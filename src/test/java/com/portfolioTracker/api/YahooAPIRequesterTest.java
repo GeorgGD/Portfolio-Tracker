6e0e5ba3334dafc5fec11a1331e2cf925e49224f
GeorgGD <giorgos_davak@hotmail.com>
2020-11-22 18:05:24 +0100

    Introduce initialize

--- src/test/java/com/portfolioTracker/api/YahooAPIRequesterTest.java
+++ src/test/java/com/portfolioTracker/api/YahooAPIRequesterTest.java
@@ -1,5 +1,20 @@
 package com.portfolioTracker.api;
 
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import okhttp3.OkHttpClient;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.portfolioTracker.api.YahooAPIRequester;
+
 public class YahooAPIRequesterTest {
-	
+
+	private YahooAPIRequester initialize() {
+		YahooAPIRequester api = new YahooAPIRequester();
+		api.setClient(new OkHttpClient());
+		api.setMapper(new ObjectMapper());
+		return api;
+	}
 }
