de3e4bdcd7976e256bedf96415e75e58dc41da9b
GeorgGD <giorgos_davak@hotmail.com>
2020-11-22 21:42:52 +0100

    Introduce setterGetterTest

--- src/test/java/com/portfolioTracker/cookies/CookieHandlerTest.java
+++ src/test/java/com/portfolioTracker/cookies/CookieHandlerTest.java
@@ -2,8 +2,24 @@ package com.portfolioTracker.cookies;
 
 import static org.junit.Assert.*;
 
+import javax.servlet.http.Cookie;
+
 import org.junit.Test;
 
 public class CookieHandlerTest {
-	
+
+	@Test
+	public void setterGetterTest() {
+		CookieHandler handler = new CookieHandler();
+		String expectedKey = "test";
+		String expectedValue = "success";
+		Cookie cookie = handler.getCookie();		
+		assertNull(cookie);
+		
+		handler.setCookie(new Cookie(expectedKey, expectedValue));
+
+		Cookie cookie2 = handler.getCookie();		
+		assertEquals(expectedKey, cookie2.getName());
+		assertEquals(expectedValue, cookie2.getValue());
+	}
 }
