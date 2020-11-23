6393b9800b2d58c6cf57e960d042f1c11af5fe9b
GeorgGD <giorgos_davak@hotmail.com>
2020-11-23 22:53:43 +0100

    Introduce PortfolioSearchTest

---
+++ src/test/java/com/portfolioTracker/integrationTests/PortfolioSearchTest.java
@@ -0,0 +1,18 @@
+package com.portfolioTracker.integrationTests;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.servlet.ModelAndView;
+
+import config.SpringConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
+public class PortfolioSearchTest {
+	
+}
