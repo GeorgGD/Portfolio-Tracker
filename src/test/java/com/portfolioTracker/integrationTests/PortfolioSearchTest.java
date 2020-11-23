214f4622de00b4175e2d9804ec5ea9b89ae7e8f4
GeorgGD <giorgos_davak@hotmail.com>
2020-11-23 23:09:54 +0100

    Introduce portfolioSearchTest

--- src/test/java/com/portfolioTracker/integrationTests/PortfolioSearchTest.java
+++ src/test/java/com/portfolioTracker/integrationTests/PortfolioSearchTest.java
@@ -1,5 +1,7 @@
 package com.portfolioTracker.integrationTests;
 
+import com.portfolioTracker.controller.PortfolioSearch;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,5 +16,18 @@ import config.SpringConfig;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringConfig.class)
 public class PortfolioSearchTest {
-	
+
+	@Autowired
+	private PortfolioSearch portfolio;
+
+	@Test
+	public void portfolioSearchTest() {
+		String ticker = "MSFT";
+		String shares = "20";
+		String buyInPrice = "5";
+		String cookie1 = "integratedTest";
+		String cookie2 = "0";
+		String cookie3 = "0";
+		ModelAndView mav = portfolio.port
+	}
 }
