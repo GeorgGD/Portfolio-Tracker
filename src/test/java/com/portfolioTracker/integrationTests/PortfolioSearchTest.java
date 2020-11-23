4ff1795a5e2bc342da03f01ad2caeafb9f1c56af
GeorgGD <giorgos_davak@hotmail.com>
2020-11-24 00:22:02 +0100

    Introduce updateEvaluationTest

--- src/test/java/com/portfolioTracker/integrationTests/PortfolioSearchTest.java
+++ src/test/java/com/portfolioTracker/integrationTests/PortfolioSearchTest.java
@@ -1,6 +1,7 @@
 package com.portfolioTracker.integrationTests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import java.util.Map;
 
@@ -29,10 +30,10 @@ public class PortfolioSearchTest {
 		String ticker = "MSFT";
 		String shares = "20";
 		String buyInPrice = "5";
-		String cookie1 = "integratedTest";
+		String username = "integratedTest";
 		String cookie2 = "0";
 		String cookie3 = "0";
-		ModelAndView mav = portfolio.portfolioSearch(ticker, shares, buyInPrice, cookie1, cookie2, cookie3);
+		ModelAndView mav = portfolio.portfolioSearch(ticker, shares, buyInPrice, username, cookie2, cookie3);
 
 		String expectedView = "portfolio";
 		assertEquals(expectedView, mav.getViewName());
@@ -43,14 +44,33 @@ public class PortfolioSearchTest {
 		String ticker = "MSFT";
 		String shares = "20";
 		String buyInPrice = "5";
-		String cookie1 = "integratedTest";
+		String username = "integratedTest";
 		String cookie2 = "0";
 		String cookie3 = "0";
-		ModelAndView mav = portfolio.portfolioSearch(ticker, shares, buyInPrice, cookie1, cookie2, cookie3);
+		ModelAndView mav = portfolio.portfolioSearch(ticker, shares, buyInPrice, username, cookie2, cookie3);
 
 		Map<String, Object> map = mav.getModel();
 		String expectedResult = "<tr><td>Microsoft Corporation</td><td>20</td><td>5 USD</td></tr>";
 		String elExpresion = "tableBody";
 		assertEquals(expectedResult, map.get(elExpresion));
+	}
+
+	@Test
+	public void updateEvaluationTest() {
+		String ticker = "MSFT";
+		String shares = "20";
+		String buyInPrice = "5";
+		String username = "integratedTest";
+		String cookie2 = "0";
+		String cookie3 = "0";
+		MockHttpServletResponse response = new MockHttpServletResponse();
+
+		portfolio.portfolioSearch(ticker, shares, buyInPrice, username, cookie2, cookie3);
+		ModelAndView mav = portfolio.updateEvaluation(username, response);
+		Map<String, Object> map = mav.getModel();
+
+		String expectedCurrentValue = "100 USD";
+		assertEquals(expectedCurrentValue, map.get("currentInvestment"));
+		assertNotEquals("0 USD", map.get("currentEvaluation"));		
 	}
 }
