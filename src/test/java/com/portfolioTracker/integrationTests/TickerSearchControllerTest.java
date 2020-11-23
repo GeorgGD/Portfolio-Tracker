32e965a99234ed6ee9a05ae3604b1b7e38bf1ba1
GeorgGD <giorgos_davak@hotmail.com>
2020-11-23 17:17:39 +0100

    Setup test to run while using spring

--- src/test/java/com/portfolioTracker/integrationTests/TickerSearchControllerTest.java
+++ src/test/java/com/portfolioTracker/integrationTests/TickerSearchControllerTest.java
@@ -1,5 +1,16 @@
 package com.portfolioTracker.integrationTests;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.servlet.ModelAndView;
+
+import config.SpringConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringConfig.class)
 public class TickerSearchControllerTest {
 	
 }
