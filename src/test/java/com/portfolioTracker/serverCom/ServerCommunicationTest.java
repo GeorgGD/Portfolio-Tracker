405e96710b7e93a5d493b59b8df64d201f6a5ae6
GeorgGD <giorgos_davak@hotmail.com>
2020-11-23 12:38:37 +0100

    Introduce readPortfolio

--- src/test/java/com/portfolioTracker/serverCom/ServerCommunicationTest.java
+++ src/test/java/com/portfolioTracker/serverCom/ServerCommunicationTest.java
@@ -1,8 +1,12 @@
 package com.portfolioTracker.serverCom;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.portfolioTracker.api.YahooAPIRequester;
@@ -16,6 +20,28 @@ public class ServerCommunicationTest {
 		server.setAPIRequester(new YahooAPIRequester());
 		server.setObjectMapper(new ObjectMapper());
 		return server;
+	}
+
+	private String readPortfolio(String username) {
+		File file = new File(username + ".txt");
+		String portfolio = "";
+		Scanner scan = null;
+		
+		try {
+			scan = new Scanner(file);
+		} catch (FileNotFoundException e1) {
+			e1.printStackTrace();
+			return "";
+		}
+		
+		while(scan.hasNextLine()) {
+			portfolio = portfolio + scan.nextLine();
+		}
+		
+		if(scan != null)
+			scan.close();
+		return portfolio;
+
 	}
 	
 	@Test
