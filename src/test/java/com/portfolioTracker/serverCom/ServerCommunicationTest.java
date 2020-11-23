86f42bfc639ea920a416cfa577780b5947390c95
GeorgGD <giorgos_davak@hotmail.com>
2020-11-23 11:57:32 +0100

    Introduce createUserTest and initialize

--- src/test/java/com/portfolioTracker/serverCom/ServerCommunicationTest.java
+++ src/test/java/com/portfolioTracker/serverCom/ServerCommunicationTest.java
@@ -1,5 +1,30 @@
 package com.portfolioTracker.serverCom;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.portfolioTracker.api.YahooAPIRequester;
+
+import org.junit.Test;
+
 public class ServerCommunicationTest {
+
+	private ServerCommunication initialize() {
+		ServerCommunication server = new ServerCommunication();
+		server.setAPIRequester(new YahooAPIRequester());
+		server.setObjectMapper(new ObjectMapper());
+		return server;
+	}
 	
+	@Test
+	public void createUserTest() {
+		ServerCommunication server = initialize();
+		String expUser = "unitTest";
+		server.createUser(expUser);
+
+		File file = new File(expUser + ".txt");
+		assertTrue(file.exists());
+	}
 }
