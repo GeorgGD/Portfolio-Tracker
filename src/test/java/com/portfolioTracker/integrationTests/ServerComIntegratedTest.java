833e9a05cb2136363d9c11b50e15df9d36f52e11
GeorgGD <giorgos_davak@hotmail.com>
2020-11-23 22:43:07 +0100

    Remove unused imports

--- src/test/java/com/portfolioTracker/integrationTests/ServerComIntegratedTest.java
+++ src/test/java/com/portfolioTracker/integrationTests/ServerComIntegratedTest.java
@@ -6,14 +6,11 @@ import java.util.HashMap;
 
 import com.portfolioTracker.serverCom.ServerCommunication;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.web.servlet.ModelAndView;
 
 import config.SpringConfig;
 
