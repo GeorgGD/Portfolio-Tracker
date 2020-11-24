b9015d1016f63c6b4e7617c802c0815c11f1eb11
GeorgGD <giorgos_davak@hotmail.com>
2020-11-24 20:23:42 +0100

    Refactor setUsernameCookie

--- src/main/java/com/portfolioTracker/controller/Username.java
+++ src/main/java/com/portfolioTracker/controller/Username.java
@@ -1,7 +1,5 @@
 package com.portfolioTracker.controller;
 
-import java.util.HashMap;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
@@ -45,31 +43,35 @@ public class Username {
 										  @CookieValue(value = "invested", defaultValue = "0") String currInvestment,
 										  @CookieValue(value = "worth", defaultValue = "0") String currWorth,
 										  HttpServletResponse response) {
-	    HashMap<String, String> expValuePair = new HashMap<String, String>();
 		String jspExpression;
 		String view;
-		ModelAndView mav;
-		viewHandler.setModelView(new ModelAndView());
+		String currency = " USD";		
+		viewHandler.newModelAndView();
 	    
 		if(username == null || username.equals("")) {
 			view = "index";
+			viewHandler.setView(view);
 			jspExpression = "errorMsg";
-			expValuePair.put(jspExpression, "Please provide a Username!");
-			mav = viewHandler.setupModelAndView(expValuePair,view);
-			return mav;
+			viewHandler.addObjectsToView(jspExpression, "Please provide a Username!");		    
+			return viewHandler.getModelView();
 		}
 		
     	cookieHandler.setCookie(new Cookie("username",username));
 		cookieHandler.oneWeekCookie();
 		response.addCookie(cookieHandler.getCookie());
-		view = "portfolio";
-		jspExpression = "currentInvestment";
-		expValuePair.put(jspExpression, currInvestment+" USD");
 
+		view = "portfolio";		
+		viewHandler.setView(view);
+		
+		jspExpression = "currentInvestment";
+		viewHandler.addObjectsToView(jspExpression, currInvestment + currency);
+		
 		jspExpression = "currentEvaluation";
-		expValuePair.put(jspExpression, currWorth+" USD");
-		expValuePair.put("tableBody", server.setupTableEntries(username));
-		mav = viewHandler.setupModelAndView(expValuePair, view);
-		return mav;
+		viewHandler.addObjectsToView(jspExpression, currWorth + currency);
+
+		jspExpression = "tableBody";
+		viewHandler.addObjectsToView(jspExpression, server.setupTableEntries(username));
+		
+	    return viewHandler.getModelView();
 	}
 }
