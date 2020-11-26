5413c3dc00246fa78ba6b025f4ed42330dc060e3
GeorgGD <giorgos_davak@hotmail.com>
2020-11-26 22:34:26 +0100

    Introduce setters

--- src/main/java/com/portfolioTracker/api/PortfolioDTO.java
+++ src/main/java/com/portfolioTracker/api/PortfolioDTO.java
@@ -4,4 +4,17 @@ public class PortfolioDTO {
 	private String ticker;
 	private String sharesNum;
 	private String buyInPrice;
+
+	public void setTicker(String ticker) {
+		this.ticker = ticker;
+	}
+
+   	public void setSharesNum(String sharesNum) {
+		this.sharesNum = sharesNum;
+	}
+
+	public void setBuyInPrice(String buyInPrice) {
+		this.buyInPrice = buyInPrice;
+	}
+	
 }
