b9abde8df26d4ff6456130f3aef797fa02bd388a
GeorgGD <giorgos_davak@hotmail.com>
2020-11-27 18:42:41 +0100

    Introduce addResourceHandler

--- src/main/java/config/SpringConfig.java
+++ src/main/java/config/SpringConfig.java
@@ -6,13 +6,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import okhttp3.OkHttpClient;
 
+@EnableWebMvc
 @Configuration
 @ComponentScan({"com.portfolioTracker"})
-public class SpringConfig {
+public class SpringConfig implements WebMvcConfigurer{
 
 	/**
 	 * The following method figures out which view the dispatcher should pick
@@ -26,6 +30,10 @@ public class SpringConfig {
 		return view;
 	}
 
+	public void addResourceHandler(final ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+	
 	@Bean
 	public OkHttpClient client() {
 		return new OkHttpClient();
