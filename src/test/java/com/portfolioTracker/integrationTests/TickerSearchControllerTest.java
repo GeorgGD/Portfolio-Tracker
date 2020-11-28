d21db9793c5056802388db9ead5958ef3cd4fcf5
GeorgGD <giorgos_davak@hotmail.com>
2020-11-28 21:07:21 +0100

    Add WebAppConfiguration annotation

--- src/test/java/com/portfolioTracker/integrationTests/TickerSearchControllerTest.java
+++ src/test/java/com/portfolioTracker/integrationTests/TickerSearchControllerTest.java
@@ -11,12 +11,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.servlet.ModelAndView;
 
 import config.SpringConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringConfig.class)
+@WebAppConfiguration
 public class TickerSearchControllerTest {
 
 	@Autowired
--- src/test/java/com/portfolioTracker/integrationTests/UsernameTest.java
+++ src/test/java/com/portfolioTracker/integrationTests/UsernameTest.java
@@ -13,12 +13,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.servlet.ModelAndView;
 
 import config.SpringConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = SpringConfig.class)
+@WebAppConfiguration
 public class UsernameTest {
 
 	@Autowired
