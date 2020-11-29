c2b5193e048c2a51da75732261c541f4d7d70ceb
GeorgGD <giorgos_davak@hotmail.com>
2020-11-29 22:50:10 +0100

    Add documentation

--- src/main/java/com/portfolioTracker/controller/Index.java
+++ src/main/java/com/portfolioTracker/controller/Index.java
@@ -7,12 +7,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+/**
+ * The following controller is meant to handling the view and data for the landing page
+ * @author Georgios Davakos
+ * @since 2020-11-29
+ */
 @Controller
 public class Index {
 
 	@Autowired
 	private ViewHandler viewHandler;
 
+	/**
+	 * Provides the view for when the visitor goes to the landing page
+	 * @return The view
+	 */
 	@RequestMapping("/")
 	public ModelAndView landingPage() {
 		viewHandler.newModelAndView();
