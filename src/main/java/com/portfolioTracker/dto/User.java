348edb35db32d2e3ac1d3c1d2c4b635caa74b53a
GeorgGD <giorgos_davak@hotmail.com>
2020-12-01 21:50:54 +0100

    Introduce private variables

--- src/main/java/com/portfolioTracker/dto/User.java
+++ src/main/java/com/portfolioTracker/dto/User.java
@@ -1,8 +1,14 @@
 package com.portfolioTracker.dto;
 
-import org.springframework.stereotype.Component;
 
-@Component
+/**
+ * The following class is meant to store the user info into a POJO 
+ * to simplify login validation
+ * @author Georgios Davakos
+ * @since 2020-12-01
+ */
 public class User {
-	
+
+	private String username;
+	private String password;	
 }
