999054906bed151d5cdf6f01351463b1e80f8431
GeorgGD <giorgos_davak@hotmail.com>
2020-12-01 21:56:02 +0100

    Add documentation

--- src/main/java/com/portfolioTracker/dto/User.java
+++ src/main/java/com/portfolioTracker/dto/User.java
@@ -12,18 +12,34 @@ public class User {
 	private String username;
 	private String password;
 
+	/**
+	 * Gets username value
+	 * @return The username	
+	 */
 	public String getUsername() {
 		return username;
 	}
 
+	/**
+	 * Sets the value for username
+	 * @param username The username value	
+	 */
 	public void setUsername(String username) {
 		this.username = username;
 	}
 
+	/**
+	 * Retrieves the password
+	 * @return The password	
+	 */
 	public String getPassword() {
 		return password;
 	}
 
+	/**
+	 * Sets the value for password
+	 * @param password The password value	
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
