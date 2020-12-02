aec08655cbc73ecc732d34b4d15a80f9914eaedb
GeorgGD <giorgos_davak@hotmail.com>
2020-12-02 13:24:48 +0100

    Remove the use of viewHandler

--- src/main/java/com/portfolioTracker/controller/Index.java
+++ src/main/java/com/portfolioTracker/controller/Index.java
@@ -1,11 +1,7 @@
 package com.portfolioTracker.controller;
 
-import com.portfolioTracker.view.ViewHandler;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * The following controller is meant to handling the view and data for the landing page
@@ -15,17 +11,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class Index {
 
-	@Autowired
-	private ViewHandler viewHandler;
-
 	/**
 	 * Provides the view for when the visitor goes to the landing page
 	 * @return The view
 	 */
 	@RequestMapping("/")
-	public ModelAndView landingPage() {
-		viewHandler.newModelAndView();
-		viewHandler.setView("index");
-		return viewHandler.getModelView();
+	public String landingPage() {
+		return "index";
 	}
 }
