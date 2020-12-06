952e24552cab91359cdcd3add1e208a5c8b8d61f
GeorgGD <giorgos_davak@hotmail.com>
2020-12-06 16:27:13 +0100

    Introduce initUser

--- src/test/java/com/portfolioTracker/serverCom/UserAuthTest.java
+++ src/test/java/com/portfolioTracker/serverCom/UserAuthTest.java
@@ -1,13 +1,28 @@
 package com.portfolioTracker.serverCom;
 
+import org.junit.Test;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.portfolioTracker.dto.User;
 
 public class UserAuthTest {
 	private String FILE_PATH = "src/main/resources/users/";
 	
 	private UserAuth initialize() {
-		UserAuth user = new UserAuth();
-		user.setMapper(new ObjectMapper());
+		UserAuth userAuth = new UserAuth();
+		userAuth.setMapper(new ObjectMapper());
+		return userAuth;
+	}
+	private User initUser() {
+		User user = new User();
+		user.setUsername("Jeo");
+		user.setPassword("dogsrock");
 		return user;
+	}
+	
+	@Test
+	public void userIsRegisteredTest() {
+		UserAuth userAuth = initialize();
+		User user = initUser();
 	}
 }
