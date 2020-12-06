856a7f7c21eaecfe86a7b9416a6346195a19c909
GeorgGD <giorgos_davak@hotmail.com>
2020-12-06 15:59:53 +0100

    Introduce initialize

--- src/test/java/com/portfolioTracker/serverCom/UserAuthTest.java
+++ src/test/java/com/portfolioTracker/serverCom/UserAuthTest.java
@@ -1,5 +1,13 @@
 package com.portfolioTracker.serverCom;
 
-public class UserAuthTest {
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+public class UserAuthTest {
+	private String FILE_PATH = "src/main/resources/users/";
+	
+	private UserAuth initialize() {
+		UserAuth user = new UserAuth();
+		user.setMapper(new ObjectMapper());
+		return user;
+	}
 }
