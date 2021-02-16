6c0c7788a5dd01d2f1cfbd58fec4b87c1c54d198
GeorgGD <giorgos_davak@hotmail.com>
2021-02-16 10:49:10 +0100

    Add documentation

--- src/main/java/com/portfolioTracker/PortfolioTrackerApp.java
+++ src/main/java/com/portfolioTracker/PortfolioTrackerApp.java
@@ -3,6 +3,12 @@ package com.portfolioTracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class for the spring boot application
+ * @author Georgios Davakos
+ * @since 2021-02-16
+ */
+
 @SpringBootApplication
 public class PortfolioTrackerApp {
 
