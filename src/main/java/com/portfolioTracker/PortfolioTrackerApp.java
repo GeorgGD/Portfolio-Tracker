8d9ab683fa7e2f09d596d1f7218621c7de724658
GeorgGD <giorgos_davak@hotmail.com>
2021-02-16 10:50:49 +0100

    Introduce beans

--- src/main/java/com/portfolioTracker/PortfolioTrackerApp.java
+++ src/main/java/com/portfolioTracker/PortfolioTrackerApp.java
@@ -1,7 +1,12 @@
 package com.portfolioTracker;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import okhttp3.OkHttpClient;
 
 /**
  * The main class for the spring boot application
@@ -14,5 +19,15 @@ public class PortfolioTrackerApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioTrackerApp.class, args);
+	}
+
+	@Bean
+	public OkHttpClient client() {
+		return new OkHttpClient();
+	}
+
+	@Bean
+	public ObjectMapper mapper() {
+		return new ObjectMapper();
 	}
 }
