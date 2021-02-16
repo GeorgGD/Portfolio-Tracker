2a855a3c9b65d7d0208fa73b6fc7267625fa8673
GeorgGD <giorgos_davak@hotmail.com>
2021-02-16 10:48:41 +0100

    Introduce PortfolioTrackerApp

---
+++ src/main/java/com/portfolioTracker/PortfolioTrackerApp.java
@@ -0,0 +1,12 @@
+package com.portfolioTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioTrackerApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioTrackerApp.class, args);
+	}
+}
