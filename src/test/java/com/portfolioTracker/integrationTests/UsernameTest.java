675f0a5aee1ace045479cb6d473eb1ebb84b9608
GeorgGD <giorgos_davak@hotmail.com>
2021-02-16 12:01:19 +0100

    Remove import and contextConfiguration annotation

--- src/test/java/com/portfolioTracker/integrationTests/UsernameTest.java
+++ src/test/java/com/portfolioTracker/integrationTests/UsernameTest.java
@@ -15,17 +15,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockHttpSession;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.ui.ExtendedModelMap;
 import org.springframework.ui.Model;
 import org.springframework.validation.MapBindingResult;
 
-import config.SpringConfig;
-
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = SpringConfig.class)
 @WebAppConfiguration
 public class UsernameTest {
 
